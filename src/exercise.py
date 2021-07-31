89e0f8e09905ccfa74e858c7d95a978f38b594f1
Justin Turner <jmturner638@gmail.com>
2021-07-30 22:50:02 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    print_text()
+
+def print_text():
+    print("In a hole in the ground there lived a method")
 
 if __name__ == '__main__':
     main()
+
+
